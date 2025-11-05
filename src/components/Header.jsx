d28ebde29a8709a8e60c52834b48e2d03de4e448
shekh moihibur rahman <mohibur.rahman2003@gmail.com>
2025-11-05 20:41:24 +0600

    added original time format in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { format } from "date-fns";
 import logo from "../assets/logo.png"
 const Header = () => {
     return (
         <div className="flex justify-center flex-col items-center gap-3">
-            <img className="w-[350]" src={logo} alt="dragon news" />
+            <img className="w-[350] mt-4" src={logo} alt="dragon news" />
             <p className="text-accent text-xl">Journalism Without Fear or Favour</p>
+            <p className="font-semibold text-accent">{ format(new Date(), 'EEEE, MMMM MM, yyyy')}</p>
         </div>
     );
 };
